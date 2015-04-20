1e4ba725b211d96d02fc04008314224ec06202a3
SkyzohKey <skyzohkey@gmail.com>
2015-04-20 14:20:03 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 // Require the lib' to work.
-require_once("classes/CryptThat.class.php");
+require_once("CryptThat.class.php");
 
 // Initialize the 3 key that the lib' will use.
 $params = array("34b1dac21df43b17d23014d42d5c25f2195170a9f101cf137b3d5975af29ec0c",
