1bcc1aec87e27ad977ca2432dd91cc3e20a3a970
SkyzohKey <SkyzohKey@users.noreply.github.com>
2015-10-16 18:59:32 +0200

    Indentation

--- example.php
+++ example.php
@@ -7,9 +7,9 @@ require_once("classes/CryptThat.class.php");
  * NOTE: You should never give these key to someone, keep them in a different file (config.php for example)
  */
 $keys = array(
-	"34b1dac21df43b17d23014d42d5c25f2195170a9f101cf137b3d5975af29ec0c",
-	"8320230184e36ced6sn764fcccc24f935cec1648450ca8d818ec6abf83dcbd39",
-	"db57830f163cc31371b720543d6613a98al32ee495ec44560441792846c4b751"
+  "34b1dac21df43b17d23014d42d5c25f2195170a9f101cf137b3d5975af29ec0c",
+  "8320230184e36ced6sn764fcccc24f935cec1648450ca8d818ec6abf83dcbd39",
+  "db57830f163cc31371b720543d6613a98al32ee495ec44560441792846c4b751"
 );
 
 // Init the lib'.
